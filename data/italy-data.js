b6e27e71dce522c90777135223d8fd635c730bea
Alexis Sacco <firstvololearning@gmail.com>
2026-07-16 13:42:25 -0400

    Updated region positions and finalized map layout

--- data/italy-data.js
+++ data/italy-data.js
@@ -1,383 +1,302 @@
 const italyData = [
-
-    {
-        id: "valle-daosta",
-        region: "Valle d'Aosta",
-        capital: "Aosta",
-
-        food: {
-            name: "",
-            image: ""
-        },
-
-        landmark: {
-            name: "",
-            image: ""
-        },
-
-        x: 12.9,
-        y: 13.3
+  {
+    id: "valle-daosta",
+    region: "Valle d'Aosta",
+    capital: "Aosta",
+    food: {
+      name: "",
+      image: ""
     },
-
-    {
-        id: "piemonte",
-        region: "Piemonte",
-        capital: "Torino",
-
-        food: {
-            name: "",
-            image: ""
-        },
-
-        landmark: {
-            name: "",
-            image: ""
-        },
-
-        x: 16.5,
-        y: 21.5
+    landmark: {
+      name: "",
+      image: ""
     },
-
-    {
-        id: "liguria",
-        region: "Liguria",
-        capital: "Genova",
-
-        food: {
-            name: "",
-            image: ""
-        },
-
-        landmark: {
-            name: "",
-            image: ""
-        },
-
-        x: 24.7,
-        y: 28.9
+    x: 9.5,
+    y: 13.3
+  },
+  {
+    id: "piemonte",
+    region: "Piemonte",
+    capital: "Torino",
+    food: {
+      name: "",
+      image: ""
     },
-
-    {
-        id: "lombardia",
-        region: "Lombardia",
-        capital: "Milano",
-
-        food: {
-            name: "",
-            image: ""
-        },
-
-        landmark: {
-            name: "",
-            image: ""
-        },
-
-        x: 31.5,
-        y: 15.5
+    landmark: {
+      name: "",
+      image: ""
     },
-
-    {
-        id: "trentino-alto-adige",
-        region: "Trentino-Alto Adige",
-        capital: "Trento",
-
-        food: {
-            name: "",
-            image: ""
-        },
-
-        landmark: {
-            name: "",
-            image: ""
-        },
-
-        x: 44.5,
-        y: 7.3
+    x: 13.5,
+    y: 20.3
+  },
+  {
+    id: "liguria",
+    region: "Liguria",
+    capital: "Genova",
+    food: {
+      name: "",
+      image: ""
     },
-
-    {
-        id: "veneto",
-        region: "Veneto",
-        capital: "Venezia",
-
-        food: {
-            name: "",
-            image: ""
-        },
-
-        landmark: {
-            name: "",
-            image: ""
-        },
-
-        x: 48.9,
-        y: 16.5
+    landmark: {
+      name: "",
+      image: ""
     },
-
-    {
-        id: "friuli-venezia-giulia",
-        region: "Friuli-Venezia Giulia",
-        capital: "Trieste",
-
-        food: {
-            name: "",
-            image: ""
-        },
-
-        landmark: {
-            name: "",
-            image: ""
-        },
-
-        x: 60.4,
-        y: 11.2
+    x: 22.9,
+    y: 28.3
+  },
+  {
+    id: "lombardia",
+    region: "Lombardia",
+    capital: "Milano",
+    food: {
+      name: "",
+      image: ""
     },
-
-    {
-        id: "emilia-romagna",
-        region: "Emilia-Romagna",
-        capital: "Bologna",
-
-        food: {
-            name: "",
-            image: ""
-        },
-
-        landmark: {
-            name: "",
-            image: ""
-        },
-
-        x: 40.5,
-        y: 25.3
+    landmark: {
+      name: "",
+      image: ""
     },
-
-    {
-        id: "toscana",
-        region: "Toscana",
-        capital: "Firenze",
-
-        food: {
-            name: "",
-            image: ""
-        },
-
-        landmark: {
-            name: "",
-            image: ""
-        },
-
-        x: 40.5,
-        y: 35.2
+    x: 29.7,
+    y: 14.7
+  },
+  {
+    id: "trentino-alto-adige",
+    region: "Trentino-Alto Adige",
+    capital: "Trento",
+    food: {
+      name: "",
+      image: ""
     },
-
-    {
-        id: "umbria",
-        region: "Umbria",
-        capital: "Perugia",
-
-        food: {
-            name: "",
-            image: ""
-        },
-
-        landmark: {
-            name: "",
-            image: ""
-        },
-
-        x: 50.4,
-        y: 39.8
+    landmark: {
+      name: "",
+      image: ""
     },
-
-    {
-        id: "marche",
-        region: "Marche",
-        capital: "Ancona",
-
-        food: {
-            name: "",
-            image: ""
-        },
-
-        landmark: {
-            name: "",
-            image: ""
-        },
-
-        x: 58.4,
-        y: 36.5
+    x: 43.3,
+    y: 7.3
+  },
+  {
+    id: "veneto",
+    region: "Veneto",
+    capital: "Venezia",
+    food: {
+      name: "",
+      image: ""
     },
-
-    {
-        id: "lazio",
-        region: "Lazio",
-        capital: "Roma",
-
-        food: {
-            name: "",
-            image: ""
-        },
-
-        landmark: {
-            name: "",
-            image: ""
-        },
-
-        x: 51.1,
-        y: 49.4
+    landmark: {
+      name: "",
+      image: ""
     },
-
-    {
-        id: "abruzzo",
-        region: "Abruzzo",
-        capital: "L'Aquila",
-
-        food: {
-            name: "Arrosticini",
-            image: "assets/foods/food-01.png"
-        },
-
-        landmark: {
-            name: "Gran Sasso",
-            image: "assets/landmarks/landmark-05.png"
-        },
-
-        x: 63.0,
-        y: 45.2
+    x: 48.9,
+    y: 15.9
+  },
+  {
+    id: "friuli-venezia-giulia",
+    region: "Friuli-Venezia Giulia",
+    capital: "Trieste",
+    food: {
+      name: "",
+      image: ""
     },
-
-    {
-        id: "molise",
-        region: "Molise",
-        capital: "Campobasso",
-
-        food: {
-            name: "",
-            image: ""
-        },
-
-        landmark: {
-            name: "",
-            image: ""
-        },
-
-        x: 67.6,
-        y: 51.6
+    landmark: {
+      name: "",
+      image: ""
     },
-
-    {
-        id: "campania",
-        region: "Campania",
-        capital: "Napoli",
-
-        food: {
-            name: "",
-            image: ""
-        },
-
-        landmark: {
-            name: "",
-            image: ""
-        },
-
-        x: 66.6,
-        y: 58.4
+    x: 60.4,
+    y: 11.2
+  },
+  {
+    id: "emilia-romagna",
+    region: "Emilia-Romagna",
+    capital: "Bologna",
+    food: {
+      name: "",
+      image: ""
     },
-
-    {
-        id: "puglia",
-        region: "Puglia",
-        capital: "Bari",
-
-        food: {
-            name: "",
-            image: ""
-        },
-
-        landmark: {
-            name: "",
-            image: ""
-        },
-
-        x: 76.7,
-        y: 53.6
+    landmark: {
+      name: "",
+      image: ""
     },
-
-    {
-        id: "basilicata",
-        region: "Basilicata",
-        capital: "Potenza",
-
-        food: {
-            name: "",
-            image: ""
-        },
-
-        landmark: {
-            name: "",
-            image: ""
-        },
-
-        x: 77.0,
-        y: 63.4
+    x: 40.3,
+    y: 25.1
+  },
+  {
+    id: "toscana",
+    region: "Toscana",
+    capital: "Firenze",
+    food: {
+      name: "",
+      image: ""
     },
-
-    {
-        id: "calabria",
-        region: "Calabria",
-        capital: "Catanzaro",
-
-        food: {
-            name: "",
-            image: ""
-        },
-
-        landmark: {
-            name: "",
-            image: ""
-        },
-
-        x: 79.4,
-        y: 73.6
+    landmark: {
+      name: "",
+      image: ""
     },
-
-    {
-        id: "sardegna",
-        region: "Sardegna",
-        capital: "Cagliari",
-
-        food: {
-            name: "",
-            image: ""
-        },
-
-        landmark: {
-            name: "",
-            image: ""
-        },
-
-        x: 20.8,
-        y: 65.0
+    x: 40.5,
+    y: 35.2
+  },
+  {
+    id: "umbria",
+    region: "Umbria",
+    capital: "Perugia",
+    food: {
+      name: "",
+      image: ""
     },
-
-    {
-        id: "sicilia",
-        region: "Sicilia",
-        capital: "Palermo",
-
-        food: {
-            name: "",
-            image: ""
-        },
-
-        landmark: {
-            name: "",
-            image: ""
-        },
-
-        x: 56.0,
-        y: 88.0
-    }
-
+    landmark: {
+      name: "",
+      image: ""
+    },
+    x: 50.4,
+    y: 39.8
+  },
+  {
+    id: "marche",
+    region: "Marche",
+    capital: "Ancona",
+    food: {
+      name: "",
+      image: ""
+    },
+    landmark: {
+      name: "",
+      image: ""
+    },
+    x: 58.4,
+    y: 36.5
+  },
+  {
+    id: "lazio",
+    region: "Lazio",
+    capital: "Roma",
+    food: {
+      name: "",
+      image: ""
+    },
+    landmark: {
+      name: "",
+      image: ""
+    },
+    x: 51.1,
+    y: 49.4
+  },
+  {
+    id: "abruzzo",
+    region: "Abruzzo",
+    capital: "L'Aquila",
+    food: {
+      name: "Arrosticini",
+      image: "assets/foods/food-01.png"
+    },
+    landmark: {
+      name: "Gran Sasso",
+      image: "assets/landmarks/landmark-05.png"
+    },
+    x: 63,
+    y: 45.2
+  },
+  {
+    id: "molise",
+    region: "Molise",
+    capital: "Campobasso",
+    food: {
+      name: "",
+      image: ""
+    },
+    landmark: {
+      name: "",
+      image: ""
+    },
+    x: 69.8,
+    y: 51.6
+  },
+  {
+    id: "campania",
+    region: "Campania",
+    capital: "Napoli",
+    food: {
+      name: "",
+      image: ""
+    },
+    landmark: {
+      name: "",
+      image: ""
+    },
+    x: 68.8,
+    y: 59.2
+  },
+  {
+    id: "puglia",
+    region: "Puglia",
+    capital: "Bari",
+    food: {
+      name: "",
+      image: ""
+    },
+    landmark: {
+      name: "",
+      image: ""
+    },
+    x: 85.3,
+    y: 58.2
+  },
+  {
+    id: "basilicata",
+    region: "Basilicata",
+    capital: "Potenza",
+    food: {
+      name: "",
+      image: ""
+    },
+    landmark: {
+      name: "",
+      image: ""
+    },
+    x: 80.2,
+    y: 63.4
+  },
+  {
+    id: "calabria",
+    region: "Calabria",
+    capital: "Catanzaro",
+    food: {
+      name: "",
+      image: ""
+    },
+    landmark: {
+      name: "",
+      image: ""
+    },
+    x: 82.6,
+    y: 73.6
+  },
+  {
+    id: "sardegna",
+    region: "Sardegna",
+    capital: "Cagliari",
+    food: {
+      name: "",
+      image: ""
+    },
+    landmark: {
+      name: "",
+      image: ""
+    },
+    x: 17.4,
+    y: 65
+  },
+  {
+    id: "sicilia",
+    region: "Sicilia",
+    capital: "Palermo",
+    food: {
+      name: "",
+      image: ""
+    },
+    landmark: {
+      name: "",
+      image: ""
+    },
+    x: 57.8,
+    y: 88.8
+  }
 ];
